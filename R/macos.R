b862b5db60080982c2b9c1ec1ae917f8e7c70350
Gábor Csárdi <csardi.gabor@gmail.com>
2020-09-23 09:59:24 +0100

    macOS docs

--- R/macos.R
+++ R/macos.R
@@ -7,23 +7,196 @@
 #' `macos_keychain_*` functions create, delete, list, lock, unlock
 #' keychains.
 #'
+#' `macos_item_classes()` lists the supported Keychain item classes.
+#' `macos_item_attr()` lists the supported attributes for these classes.
+#' `macos_item_match_options()` lists the options supported by the
+#' `match` argument of `macos_item_search()`.
+#'
 #' @details
-#' ## Keychain items
+#' # Keychain items
 #'
-#' TODO
+#' `macos_item_classes()` returns the currently supported Keychain item
+#' classes.
 #'
-#' ### Attributes for different Keychain item classes
+#' ```{r}
+#' macos_item_classes()
+#' ```
 #'
-#' TODO
+#' `macos_item()` creates a new Keychain item. See the next section about
+#' the attributes that are supported for the various item types.
 #'
-#' ## Keychains
+#' ```{r}
+#' it <- macos_item("secret", list(service = "My service", account = "Gabor"))
+#' it
+#' ```
 #'
+#' `macos_item_add()` adds an item to the keychain. If there is already an
+#' item with the same primary keys, then it will error.
 #'
-#' TODO
+#' ```{r, include = FALSE}
+#' # Remove, to make sure that 'add' works
+#' tryCatch(
+#'   macos_item_delete(attributes = list(service = "My service")),
+#'   error = function(e) NULL
+#' )
+#' ```
 #'
+#' ```{r}
+#' macos_item_add(it)
+#' ```
+#'
+#' `macos_item_search()` searches for Keychain items. If `return_data` is
+#' `TRUE` then it also returns the secret data. Returning the secret data
+#' might create a password entry dialog. If `return_data` is `TRUE` then
+#' you need to set the `limit` match condition to a (small) finite number.
+#'
+#' ```{r}
+#' macos_item_search(attributes = list(service = "My service"))
+#' ```
+#'
+#' `macos_item_update()` updates existing Keychain items.
+#'
+#' ```{r}
+#' macos_item_update(
+#'   attributes = list(service = "My service", account = "Gabor"),
+#'   update = list(account = "Gabor Csardi")
+#' )
+#' macos_item_search(attributes = list(service = "My service"))
+#' ```
+#'
+#' `macos_item_delete()` deletes one or more Keychain items. Note that
+#' all matching items will be deleted.
+#'
+#' ```{r}
+#' macos_item_delete(attributes = list(service = "My service"))
+#' macos_item_search(attributes = list(service = "My service"))
+#' ```
+#'
+#' ## Keychain Item Attributes
+#'
+#' * The set of supported attributes depends on the class of the item.
+#' * oskeyring supports the following item classes currently:
+#'   `r paste(macos_item_classes(), collapse = ", ")`.
+#' * A subset of the attributes form a _primary key_. It is not possible
+#'   to add more than one item with the same primary key. See the
+#'   primary keys for the various classes below.
+#' * oskeyring does not currently support all attributes that the
+#'   Keychain Services AIP supports.
+#' * Some attributes are read-only. If you try to set them when adding
+#'   or updating items, they will be ignored.
+#' * If an attribute is not included in the return value of
+#'   `macos_item_search()` then it is not set, and its default value is in
+#'   effect.
+#'
+#' ### Attributes for generic passwords
+#'
+#' `r item_list(macos_item_attr()[["generic_password"]])`
+#'
+#' ### Attributes for internet passwords
+#'
+#' `r item_list(macos_item_attr()[["internet_password"]])`
+#'
+#' # Search Parameters
+#'
+#' osxkeychain only supports a limited set of search parameters.
+#' You can provide these for `macos_item_search()` as the `match` argument:
+#'
+#' `r item_list(macos_item_match_options())`
+#'
+#' # Keychains
+#'
+#' macOs supports multiple keychains.
+#' There is always a default keychain, which is the user's login keychain,
+#' unless configured differently.
+#' There is also a keychain search list.
+#' Keychains may belong into four non-exclusive categories, see the
+#' `domain` argument of `macos_keychain_list()`.
+#' A keychain is stored in an encrypted file on the disk, see the first
+#' column of the output of `macos_keychain_list()`.
+#'
+#' `macos_item_*()` functions have a `keychain` attribute to direct or
+#' restrict the operation to a single keychain only. These are the defaults:
+#' * `macos_item_add()` adds the item to the default keychain.
+#' * `macos_item_search()` searches all keychains in the search list.
+#' * `macos_item_update()` updates matching items on all keychains in the
+#'   search list.
+#' * `macos_item_delete()` deletes matching items from all keychains in the
+#'   search list.
+#'
+#' `macos_keychain_create()` creates a new keychain.
+#'
+#' `macos_keychain_list()` lists all keychains on the search list.
+#'
+#' ```r
+#' new <- "~/Library/Keychains/test.keychain-db"
+#' macos_keychain_create(new, password = "secret")
+#' macos_keychain_list()
+#' ```
+#'
+#' ```r
+#' ##                                                     path is_unlocked
+#' ## 1 /Users/gaborcsardi/Library/Keychains/login.keychain-db        TRUE
+#' ## 2 /Users/gaborcsardi/Library/Keychains/shiny.keychain-db       FALSE
+#' ## 3  /Users/gaborcsardi/Library/Keychains/test.keychain-db        TRUE
+#' ## 4                     /Library/Keychains/System.keychain       FALSE
+#' ##   is_readable is_writeable
+#' ## 1        TRUE         TRUE
+#' ## 2        TRUE        FALSE
+#' ## 3        TRUE         TRUE
+#' ## 4        TRUE        FALSE
+#' ```
+#'
+#' `macos_keychain_lock()` locks a keychain.
+#' `macos_keychain_unlock()` unlocks a keychain.
+#' `macos_keychain_is_locked()` checks if a keychain is locked.
+#'
+#' ```r
+#' macos_keychain_lock(new)
+#' macos_keychain_is_locked(new)
+#' ```
+#'
+#' ```r
+#' ## [1] TRUE
+#' ```
+#'
+#' ```r
+#' macos_keychain_unlock(new, password = "secret")
+#' macos_keychain_is_locked(new)
+#' ```
+#'
+#' ```r
+#' ## [1] FALSE
+#' ```
+#'
+#' `macos_keychain_delete()` deletes a keychain: it removes it from the
+#' search list and deletes the data from the disk. It currently refuses to
+#' delete the user's login keychain and the system keychain. Use Keychain
+#' Access instead if you want to delete these. (Only do this if you are
+#' aware of the bad consequences.)
+#'
+#' ```r
+#' macos_keychain_delete(new)
+#' macos_keychain_list()
+#' ```
+#'
+#' ```r
+#' ##                                                     path is_unlocked
+#' ## 1 /Users/gaborcsardi/Library/Keychains/login.keychain-db        TRUE
+#' ## 2 /Users/gaborcsardi/Library/Keychains/shiny.keychain-db       FALSE
+#' ## 3                     /Library/Keychains/System.keychain       FALSE
+#' ##   is_readable is_writeable
+#' ## 1        TRUE         TRUE
+#' ## 2        TRUE        FALSE
+#' ## 3        TRUE        FALSE
+#' ```
+#'
+#' @seealso The Keychain Services API documentation at
+#' <https://developer.apple.com/documentation/security/keychain_services>.
 #' @export
 #' @rdname macos_keychain
 #' @name macos_keychain
+#' @examples
+#' # See above
 
 macos_item_classes <- function() {
   # TODO: support the rest
@@ -102,11 +275,6 @@ macos_item_add <- function(item, keychain = NULL) {
   invisible(call_with_cleanup(oskeyring_macos_add, item, keychain))
 }
 
-#' @details
-#' ## Search Parameters
-#'
-#' TODO
-#'
 #' @param class Type of items to search, see [macos_item_classes()] for
 #' possible values.
 #' @param attributes Narrow the search by indicating the attributes that
@@ -201,7 +369,7 @@ macos_keychain_create <- function(keychain, password = NULL) {
 
   password <- password %||% ask_pass("Keychain password: ")
   file <- macos_keychain_file(keychain)
-  call_with_cleanup(oskeyring_macos_keychain_create, file, password)
+  invisible(call_with_cleanup(oskeyring_macos_keychain_create, file, password))
 }
 
 #' @param domain The preference domain from which you wish to retrieve
@@ -234,7 +402,7 @@ macos_keychain_list <- function(domain = c("all", "user", "system",
 
 macos_keychain_delete <- function(keychain) {
   stopifnot(
-    is_string(keychain),
+    is_string(keychain)
   )
   if (grepl("^login\\.keychain", basename(keychain))) {
     stop("Refusing to delete the login keychain")
@@ -243,7 +411,7 @@ macos_keychain_delete <- function(keychain) {
     stop("Refusing to delete the system keychain")
   }
   file <- macos_keychain_file(keychain)
-  call_with_cleanup(oskeyring_macos_keychain_delete, file)
+  invisible(call_with_cleanup(oskeyring_macos_keychain_delete, file))
 }
 
 #' @export
@@ -254,7 +422,7 @@ macos_keychain_lock <- function(keychain = NULL) {
     is_string(keychain) || is.null(keychain)
   )
   file <- macos_keychain_file(keychain)
-  call_with_cleanup(oskeyring_macos_keychain_lock, file)
+  invisible(call_with_cleanup(oskeyring_macos_keychain_lock, file))
 }
 
 #' @export
@@ -268,7 +436,7 @@ macos_keychain_unlock <- function(keychain = NULL, password = NULL) {
 
   password <- password %||% ask_pass("Password to unlock keychain: ")
   file <- macos_keychain_file(keychain)
-  call_with_cleanup(oskeyring_macos_keychain_unlock, file, password)
+  invisible(call_with_cleanup(oskeyring_macos_keychain_unlock, file, password))
 }
 
 #' @export
@@ -276,7 +444,7 @@ macos_keychain_unlock <- function(keychain = NULL, password = NULL) {
 
 macos_keychain_is_locked <- function(keychain = NULL) {
   stopifnot(
-    is_string(keychain) || is.null(keychain),
+    is_string(keychain) || is.null(keychain)
   )
   file <- macos_keychain_file(keychain)
   call_with_cleanup(oskeyring_macos_keychain_is_locked, file)
@@ -285,19 +453,7 @@ macos_keychain_is_locked <- function(keychain = NULL) {
 #' @export
 #' @rdname macos_keychain
 
-macos_keychain_file <- function(keychain = NULL) {
-  if (!is.null(keychain)) {
-    enc2utf8(normalizePath(keychain, mustWork = FALSE))
-  }
-}
-
-# ------------------------------------------------------------------------
-# Internals
-# ------------------------------------------------------------------------
-
-# TODO: create these from the C data to avoid duplication
-
-macos_attr <- function() {
+macos_item_attr <- function() {
   list(
     generic_password = list(
 #      access,
@@ -374,18 +530,10 @@ macos_attr <- function() {
   )
 }
 
-is_macos_attributes <- function(attr, class) {
-  if (!is_named_list(attr)) return(FALSE)
-  attr <- macos_attr()[[class]]
-  bad <- setdiff(names(attributes), names(attr))
-  if (length(bad)) {
-    stop("Unknown attributes for `", class, "`:",
-         paste0("`", bad, "`", collapse = ", "))
-  }
-  TRUE
-}
+#' @export
+#' @rdname macos_keychain
 
-macos_match_options <- function() {
+macos_item_match_options <- function() {
   list(
     ## TODO: authentication_context,
     ## TODO: how does use_authentication_ui work?
@@ -397,27 +545,49 @@ macos_match_options <- function() {
     ## subject_starts_with,
     ## subject_ends_with,
     ## subject_whole_string,
-    case_insensitive = paste0(
-      "[logical(1)] If this value is `TRUE`, or if this option is not ",
-      "provided, then case-sensitive string matching is performed"),
-    diacritic_insensitive = paste0(
-      "[logical(1)] If this value is `FALSE`, or if this option is not ",
-      "provided, then diacritic-sensitive string matching is performed."),
-    width_insensitive = paste0(
-      "[logical(1)] If this value is `FALSE`, or if this option is not ",
-      "provided, then width-sensitive string matching is performed."),
+    ## TODO: case_insensitive does not work? Only for certs?
+    ## TODO: diacritic_insensitive similar
+    ## TODO: width_insensitive similar
     ## trusted_only,
     ## valid_on_date,
     limit = paste0(
-      "[logical(1)] This value specifies the maximum number of results ",
+      "[numeric(1)] This value specifies the maximum number of results ",
       "to return or otherwise act upon. Use `Inf` to specify all ",
-      "matching items")
+      "matching items.")
   )
+}
+
+# ------------------------------------------------------------------------
+# Internals
+# ------------------------------------------------------------------------
+
+macos_keychain_file <- function(keychain = NULL) {
+  if (!is.null(keychain)) {
+    enc2utf8(normalizePath(keychain, mustWork = FALSE))
+  }
+}
+
+item_list <- function(x) {
+  nms <- names(x)
+  x <- gsub("[", "\\[", x, fixed = TRUE)
+  x <- gsub("]", "\\]", x, fixed = TRUE)
+  paste0("* `", nms, "`: ", x, collapse = "\n")
+}
+
+is_macos_attributes <- function(attr, class) {
+  if (!is_named_list(attr)) return(FALSE)
+  attr <- macos_item_attr()[[class]]
+  bad <- setdiff(names(attributes), names(attr))
+  if (length(bad)) {
+    stop("Unknown attributes for `", class, "`:",
+         paste0("`", bad, "`", collapse = ", "))
+  }
+  TRUE
 }
 
 is_macos_match <- function(x) {
   if (!is_named_list(x)) return(FALSE)
-  mtch <- macos_match_options()
+  mtch <- macos_item_match_options()
   bad <- setdiff(names(x), names(mtch))
   if (length(bad)) {
     stop("Unknown attributes for `", class, "`:",
