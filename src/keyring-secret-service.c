020871afd6f91e2d4d2066ac05c47e8c7f48dc85
Gábor Csárdi <csardi.gabor@gmail.com>
2020-10-21 23:47:11 +0100

    Add C init function on Linux, Solaris, etc.
    Otherwise `R CMD check` freaks out.

--- src/keyring-secret-service.c
+++ src/keyring-secret-service.c
@@ -2,8 +2,33 @@
 /* Avoid warning about empty compilation unit. */
 void keyring_secret_service_dummy() { }
 
-#ifdef __linux__
+#ifndef _WIN32
+#ifndef __APPLE__
 
-// TODO
 
-#endif // __linux__
+#define R_NO_REMAP 1
+
+#include "oskeyring.h"
+#include "cleancall.h"
+
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+
+#define REGISTER(method, args) \
+{ #method, (DL_FUNC) &method, args }
+
+static const R_CallMethodDef callMethods[]  = {
+  CLEANCALL_METHOD_RECORD,
+
+  { NULL, NULL, 0 }
+};
+
+void R_init_oskeyring(DllInfo *dll) {
+  R_registerRoutines(dll, NULL, callMethods, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+  R_forceSymbols(dll, TRUE);
+  cleancall_init();
+}
+
+#endif // __APPLE__
+#endif // _WIN32
